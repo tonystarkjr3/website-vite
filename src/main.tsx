1b426975780fa1eb7b096fd8ba9ac054e0b7107e
Anupam Pokharel <apokhare@alumni.cmu.edu>
2025-10-12 17:28:23 -0500

    change base to /

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/my-portfolio/">
+  <BrowserRouter basename="/">
     <App />
   </BrowserRouter>
 );
